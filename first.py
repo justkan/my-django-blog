e1fb1df9a98ff701475493f37f2ec143a392437e
justkan <justyna.ka92@gmail.com>
2018-06-09 11:14:17 +0200

    DjangoGirls app, first commit

---
+++ first.py
@@ -0,0 +1,3 @@
+print('Hello Justyna how are you?')
+a=1+2
+print('Twoj szczesliwy numerek to:', a)
---
+++ myfirstblog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Myfirstpost
+
+admin.site.register(Myfirstpost)
+
+# Register your models here.
